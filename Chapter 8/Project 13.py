d4bb313b752ed238132f68116de5049d32e8f36c
Zidna Fatha Nazhifa <89819398+zidnafatha@users.noreply.github.com>
2021-11-20 22:13:19 +0700

    Update Project 13.py

--- Chapter 8/Project 13.py
+++ Chapter 8/Project 13.py
@@ -18,6 +18,12 @@ def nilaiakhir(nilaiMhs):
             break
     return akhir
 
+nilaiMhs = [{'nim':'A01','nama':'Amir','mid':50,'uas':80},
+            {'nim':'A02','nama':'Budi','mid':40,'uas':90},
+            {'nim':'A03','nama':'Cici','mid':50,'uas':50},
+            {'nim':'A04','nama':'Dedi','mid':20,'uas':30},
+            {'nim':'A05','nama':'Fifi','mid':70,'uas':40}]
+
 i = 0
 while True:
     print (nilaiMhs[i])
@@ -25,5 +31,30 @@ while True:
     if i==5:
         break
 
-print('Mahasiswa dengan nilai tertinggi adalah ', end='')
-nilaiakhir(nilaiMhs)
+def carinilaitertinggi(list):
+    nilaiakhir=[]
+    nomorindex=[]
+    i=0
+    for data in list:
+        mid= data['mid']
+        uas= data['uas']
+        x= (mid+2*uas)/3
+        nilaiakhir+=[round(x)]
+        
+    for x in nilaiakhir:
+        tertinggi=max(nilaiakhir)
+        if x==tertinggi:
+            nomorindex+=[i]
+            break
+        i+=1
+
+    for i in nomorindex:
+        print('Mahasiswa dengan nilai tertinggi adalah', list[i]['nama'],'('+list[i]['nim']+')')
+
+i = 0
+while True:
+    print (nilaiMhs[i])
+    i+=1
+    if i==5:
+        break
+carinilaitertinggi(nilaiMhs)
